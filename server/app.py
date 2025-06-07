ac859701e53a93bfe70788a6f5a75d54a5e34ecd
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-06-08 00:56:41 +0300

    Use Flask-SQLAlchemy to define a data model, Use SQLAlchemy-Serializer to serialize an object,Implement a Flask application that queries the database and also returns JSON response.

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,46 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquakes_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    
+    if earthquake:
+        body = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }      
+        status_code = 200
+    else:
+        body = {
+            "message": f"Earthquake {id} not found."
+        }
+        status_code = 404
+    
+    return make_response(jsonify(body), status_code)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    matching_quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    quakes_data = [
+        {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        for quake in matching_quakes
+    ]
+
+    body = {
+        "count": len(quakes_data),
+        "quakes": quakes_data
+    }
+
+    return make_response(jsonify(body), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
